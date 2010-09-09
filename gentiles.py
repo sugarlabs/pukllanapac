a37532dc04b42aa05517559946f0a6596a1bbb64
Walter Bender <walter@sugarlabs.org>
2010-09-09 13:47:39 -0400

    adding artwork for triangle game

--- gentiles.py
+++ gentiles.py
@@ -13,7 +13,7 @@
 # Boston, MA 02111-1307, USA.
 
 import os
-from constants import C, H, RT, LT
+from constants import C, H, RT
 
 XCOR = ["-188.17646", "11.823541"]
 YCOR = ["162.71119", "362.71119"]
@@ -108,33 +108,48 @@ def _svg_hex(top, left, right):
     return a + b + c + d + e + f + g + h + i + j + k + l + m + n + o + p + q +\
         r + s + t + u
 
-def _svg_triangle(top, bottom, left, flip=False):
+def _svg_triangle(top, bottom, left, rotate=None):
     a = '<?xml version="1.0" encoding="UTF-8" standalone="no"?>\n'
     b = '<!-- Created with Python in Emacs -->\n'
     c = '<svg\n'
     d = '   xmlns:svg="http://www.w3.org/2000/svg"\n'
     e = '   xmlns="http://www.w3.org/2000/svg"\n'
     f = '   version="1.1"\n'
-    g = '   id="triange">\n'
-    if flip:
-        h = '  <g     transform="matrix(-1,0,0,1,838.68554,-456.19895)">\n'
+    g = '   width="103.6" height="117.8">\n'
+    if rotate == 60:
+        rr1 = '<g\n     transform="matrix(0.5,0.87,-0.87,0.5,101.3,-2.4)">\n'
+        rr2 = '</g>'
+    elif rotate == 120:
+        rr1 = '<g\n     transform="matrix(-0.5,0.87,-0.87,-0.5,105.1,58.0)">\n'
+        rr2 = '</g>'
+    elif rotate == 240:
+        rr1 = '<g\n     transform="matrix(-0.5,-0.87,0.87,-0.5,1.7,118.7)">\n'
+        rr2 = '</g>'
+    elif rotate == 300:
+        rr1 = '<g\n     transform="matrix(0.5,-0.87,0.87,0.5,-1.0,59.6)">\n'
+        rr2 = '</g>'
     else:
-        h = '  <g     transform="translate(-735.12125,-456.19895)">\n'
-    i = '    <g      transform="translate(129.81785,111.40982)">\n'
+        rr1 = ''
+        rr2 = ''
+    if rotate == 180:
+        h = '  <g     transform="matrix(-1,0,0,-1,838.7,574.0)">\n'
+    else:
+        h = '  <g     transform="translate(-735.1,-456.2)">\n'
+    i = '    <g      transform="translate(129.8,111.4)">\n'
     j = '      <path\n'
-    k = '         d="m 607.82135,460.38159 98.22306,-54.95995 -65.57024,-0.92468 -32.65282,55.88463 z"\n'
+    k = '         d="m 607.8,460.4 98.2,-55.0 -65.6,-0.9 -32.7,55.9 z"\n'
     l = '         style="fill:' + bottom + ';fill-opacity:1;stroke:#000000;stroke-width:4;stroke-linecap:butt;stroke-linejoin:round;stroke-miterlimit:4;stroke-opacity:1;stroke-dasharray:none" />\n'
     m = '      <path\n'
-    n = '         d="m 607.3034,460.58514 0,-113.47935 33.29261,56.63592 -33.29261,56.84343 z"\n'
-    o = '         style="fill:' + left + ';fill-opacity:1;stroke:#000000;stroke-width:4;stroke-linecap:butt;stroke-linejoin:round;stroke-miterlimit:4;stroke-opacity:1;stroke-dasharray:none" />'
+    n = '         d="m 607.3,460.6 0,-113.5 33.3,56.7 -33.3,56.8 z"\n'
+    o = '         style="fill:' + left + ';fill-opacity:1;stroke:#000000;stroke-width:4;stroke-linecap:butt;stroke-linejoin:round;stroke-miterlimit:4;stroke-opacity:1;stroke-dasharray:none" />\n'
     p = '      <path\n'
-    q = '         d="m 706.86769,405.18383 -98.70613,-58.3947 32.3358,57.2359 66.37033,1.1588 z"\n'
+    q = '         d="m 706.9,405.2 -98.7,-58.4 32.3,57.2 66.4,1.2 z"\n'
     r = '         style="fill:' + top + ';fill-opacity:1;stroke:#000000;stroke-width:4;stroke-linecap:butt;stroke-linejoin:round;stroke-miterlimit:4;stroke-opacity:1;stroke-dasharray:none" />\n'
     s = '    </g>\n'
     t = '</g>\n'
     u = '</svg>\n'
-    return a + b + c + d + e + f + g + h + i + j + k + l + m + n + o + p + q +\
-        r + s + t + u
+    return a + b + c + d + e + f + g + rr1 + h + i + j + k + l + m + n + o + \
+        p + q + r + s + t + rr2 + u
 
 def open_file(datapath, filename):
     """ Create a file for writing """
@@ -160,13 +175,23 @@ def generator(datapath):
         f.close()
 
     for i, c in enumerate(RT):
-        f = open_file(datapath, "triangle-r-%d.svg" % (i))
+        f = open_file(datapath, "triangle-r0-%d.svg" % (i))
         f.write(_svg_triangle(c[0], c[1], c[2]))
         f.close()
-
-    for i, c in enumerate(LT):
-        f = open_file(datapath, "triangle-l-%d.svg" % (i))
-        f.write(_svg_triangle(c[0], c[1], c[2], flip=True))
+        f = open_file(datapath, "triangle-r60-%d.svg" % (i))
+        f.write(_svg_triangle(c[0], c[1], c[2], rotate=60))
+        f.close()
+        f = open_file(datapath, "triangle-r120-%d.svg" % (i))
+        f.write(_svg_triangle(c[0], c[1], c[2], rotate=120))
+        f.close()
+        f = open_file(datapath, "triangle-r180-%d.svg" % (i))
+        f.write(_svg_triangle(c[0], c[1], c[2], rotate=180))
+        f.close()
+        f = open_file(datapath, "triangle-r240-%d.svg" % (i))
+        f.write(_svg_triangle(c[0], c[1], c[2], rotate=240))
+        f.close()
+        f = open_file(datapath, "triangle-r300-%d.svg" % (i))
+        f.write(_svg_triangle(c[0], c[1], c[2], rotate=300))
         f.close()
 
 def main():
